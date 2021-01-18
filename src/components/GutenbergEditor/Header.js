3d33bfbc7430ec46bbe557f6f26bbbb883c4d863
Mainul Hassan Main <shamim.moeen@gmail.com>
2021-01-18 21:23:29 +0600

    Change in Gutenberg editor #34

--- src/components/GutenbergEditor/Header.js
+++ src/components/GutenbergEditor/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { __ } from '@wordpress/i18n';
 import { BlockNavigationDropdown, Inserter, ToolSelector } from '@wordpress/block-editor';
@@ -6,26 +6,21 @@ import { Button } from '@wordpress/components';
 import Icon from '@mdi/react';
 import { mdiCloseCircle } from '@mdi/js';
 
-import { isActiveCellChanged, updateTable, updateTableWithCellData } from '../../utils/table';
-import { toastError } from '../../utils/utils';
+import { isActiveCellChanged, updateTableWithCellData } from '../../utils/table';
 import { TABLE } from '../../utils/views';
 import { setView } from '../../store/reducers/ui';
 import {
 	setIsBlocksChanged,
 	setTable,
 	unsetActiveCell,
-	unsetIsBlocksChanged,
-	unsetIsChanged
+	unsetIsBlocksChanged
 } from '../../store/reducers/table';
 import { setTableChangedDialog, unsetTableChangedDialog } from '../../store/reducers/dialogs';
-import { setCache, setTables } from '../../store/reducers/tables';
 
 const Header = () => {
 	const dispatch = useDispatch();
-	const { tables, cache } = useSelector( state => state.tables );
 	const tableState = useSelector( state => state.table );
 	const { table, activeCell, isBlocksChanged } = tableState;
-	const [ btnBusy, setBtnBusy ] = useState( false );
 
 	useEffect( () => {
 		if ( isActiveCellChanged( table, activeCell ) ) {
@@ -36,54 +31,11 @@ const Header = () => {
 	}, [ activeCell ] );
 
 	const onHandleSave = () => {
-		if ( btnBusy ) {
-			return;
-		}
-
-		setBtnBusy( true );
-
-		const oldTables = [ ...tables ];
 		const updatedTable = updateTableWithCellData( table, activeCell );
 
-		const newTables = oldTables.map( ( item ) => {
-			if ( item.id === updatedTable.id ) {
-				return updatedTable;
-			}
-
-			return item;
-		} );
-
-		const oldCache = [ ...cache ];
-
-		const newCache = oldCache.map( ( item ) => {
-			if ( item.id === updatedTable.id ) {
-				return updatedTable;
-			}
-
-			return item;
-		} );
-
-		// eslint-disable-next-line camelcase
-		const { id, advanced_wp_table_data } = updatedTable;
-		const title = updatedTable.title.rendered;
-
-		updateTable( id, title, advanced_wp_table_data )
-			.then( () => {
-				dispatch( setTable( updatedTable ) );
-				dispatch( setTables( newTables ) );
-				dispatch( setCache( newCache ) );
-				dispatch( unsetIsChanged() );
-				dispatch( unsetIsBlocksChanged() );
-				setBtnBusy( false );
-			} )
-			.catch( () => {
-				dispatch( setTable( table ) );
-				dispatch( setTables( oldTables ) );
-				dispatch( setCache( oldCache ) );
-				setBtnBusy( false );
-
-				toastError( __( 'Oops, there was a problem when updating the table', 'advanced-wp-table' ) );
-			} );
+		dispatch( setTable( updatedTable ) );
+		dispatch( setView( TABLE ) );
+		dispatch( unsetActiveCell() );
 	};
 
 	const navigateToTable = () => {
@@ -112,12 +64,6 @@ const Header = () => {
 		}
 	};
 
-	let btnTitle = __( 'Save', 'advanced-wp-table' );
-
-	if ( btnBusy ) {
-		btnTitle = __( 'Saving...', 'advanced-wp-table' );
-	}
-
 	return (
 		<div className={ 'advanced-wp-table-editor-header' }>
 			<div className={ 'advanced-wp-table-editor-toolbar' }>
@@ -129,18 +75,15 @@ const Header = () => {
 				<Button
 					isPrimary
 					onClick={ onHandleSave }
-					isBusy={ btnBusy }
-					aria-disabled={ btnBusy }
 					disabled={ ! isBlocksChanged }
 				>
-					{ btnTitle }
+					{ __( 'Save Changes', 'advanced-wp-table' ) }
 				</Button>
 				<Button
 					className={ 'has-icon close-btn' }
 					onClick={ onHandleCloseModal }
 					isDestructive
 					data-testid={ 'close-btn' }
-					disabled={ btnBusy }
 				>
 					<Icon path={ mdiCloseCircle } size={ '20px' } />
 				</Button>
