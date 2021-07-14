487d9cb90e36a8f915702aa1b1c688204e3b18be
Mainul Hassan Main <shamim.moeen@gmail.com>
2021-07-14 12:59:54 +0600

    Refactor the Table.js component by using hook

--- src/components/Views/Table.js
+++ src/components/Views/Table.js
@@ -1,31 +1,29 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { offsetIndex } from '../../utils/utils';
 import Header from '../Table/Header';
 import TableCell from '../Table/TableCell';
-import { setIsChanged, setTable, unsetIsChanged } from '../../store/reducers/table';
+import { setIsChanged, unsetIsChanged } from '../../store/reducers/table';
 import { isTableChanged } from '../../utils/table';
-
-// tell direction after drag start, the first direction that reach 5px offset
-const DRAG_DIRECTION_NONE = '';
-const DRAG_DIRECTION_ROW = 'row';
-const DRAG_DIRECTION_COLUMN = 'column';
-
-const defaultDragState = {
-	column: -1,
-	row: -1,
-	startPoint: null,
-	direction: DRAG_DIRECTION_NONE, // row=move up down/column=move left right,
-	dropIndex: -1, // drag target
-};
+import useDraggableTable from '../../hooks/useDraggableTable';
 
 const Table = () => {
 	const dispatch = useDispatch();
 	const { tables } = useSelector( state => state.tables );
 	const { table } = useSelector( state => state.table );
-	const { advanced_wp_table_data: tableData } = table;
+
 	const toolbarRef = useRef( null );
+	const rowsEl = useRef( null );
+
+	const [
+		rowsRef,
+		preview,
+		rows,
+		style,
+		onDragStart,
+		onDragEnter,
+		onDragEnd,
+	] = useDraggableTable( table );
 
 	useEffect( () => {
 		if ( isTableChanged( tables, table ) ) {
@@ -34,61 +32,6 @@ const Table = () => {
 			dispatch( unsetIsChanged() );
 		}
 	}, [ table ] );
-
-	let { rows = [] } = tableData;
-
-	const [ dragState, setDragState ] = useState( { ...defaultDragState } );
-	const rowsEl = useRef( null ),
-		rowsRef = useRef( [] ),
-		preview = useRef( null );
-
-	if ( dragState.direction === DRAG_DIRECTION_COLUMN ) {
-		rows = rows.map( ( x ) => offsetIndex( dragState.column, dragState.dropIndex, x ) );
-	}
-
-	if ( dragState.direction === DRAG_DIRECTION_ROW ) {
-		rows = offsetIndex( dragState.row, dragState.dropIndex, rows );
-	}
-
-	const setOpacity = ( i, j ) => {
-		let opacity = null;
-		const { direction, dropIndex } = dragState;
-
-		if ( DRAG_DIRECTION_COLUMN === direction ) {
-			if ( j === dropIndex ) {
-				opacity = 0.5;
-			} else {
-				opacity = 1;
-			}
-		}
-
-		if ( DRAG_DIRECTION_ROW === direction ) {
-			if ( i === dropIndex ) {
-				opacity = 0.5;
-			} else {
-				opacity = 1;
-			}
-		}
-
-		return opacity;
-	};
-
-	// https://stackoverflow.com/a/56063129/2647905
-	useEffect( () => {
-		rowsRef.current = rowsRef.current.slice( 0, rows.length );
-	}, [ rows ] );
-
-	const updateRows = ( rows ) => {
-		const updatedTable = {
-			...table,
-			advanced_wp_table_data: {
-				...table.advanced_wp_table_data,
-				rows
-			}
-		};
-
-		dispatch( setTable( updatedTable ) );
-	};
 
 	return (
 		<Fragment>
@@ -101,66 +44,11 @@ const Table = () => {
 								<td
 									className={ 'advanced-wp-table-cell' }
 									key={ j }
-									style={ {
-										cursor: dragState.direction ? 'move' : 'initial',
-										opacity: setOpacity( i, j ),
-									} }
 									draggable="true"
-									onDragStart={ ( e ) => {
-										e.dataTransfer.setDragImage( preview.current, 0, 0 );
-										setDragState( {
-											...dragState,
-											row: i,
-											column: j,
-											startPoint: {
-												x: e.pageX,
-												y: e.pageY,
-											},
-										} );
-									} }
-									onDragEnter={ () => {
-										if ( ! dragState.direction ) {
-											if ( dragState.column !== j ) {
-												setDragState( {
-													...dragState,
-													direction: DRAG_DIRECTION_COLUMN,
-													dropIndex: j,
-												} );
-												return;
-											}
-											if ( dragState.row !== i ) {
-												setDragState( {
-													...dragState,
-													direction: DRAG_DIRECTION_ROW,
-													dropIndex: i,
-												} );
-												return;
-											}
-											return;
-										}
-
-										if ( dragState.direction === DRAG_DIRECTION_COLUMN ) {
-											if ( j !== dragState.dropIndex ) {
-												setDragState( {
-													...dragState,
-													dropIndex: j,
-												} );
-											}
-											return;
-										}
-										if ( dragState.direction === DRAG_DIRECTION_ROW ) {
-											if ( i !== dragState.dropIndex ) {
-												setDragState( {
-													...dragState,
-													dropIndex: i,
-												} );
-											}
-										}
-									} }
-									onDragEnd={ () => {
-										updateRows( rows );
-										setDragState( { ...defaultDragState } );
-									} }
+									style={ style( i, j ) }
+									onDragStart={ ( e ) => onDragStart( e, i, j ) }
+									onDragEnter={ () => onDragEnter( i, j ) }
+									onDragEnd={ onDragEnd }
 								>
 									<TableCell
 										i={ i }
---
+++ src/hooks/useDraggableTable.js
@@ -0,0 +1,157 @@
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { offsetIndex } from '../utils/utils';
+import { setTable } from '../store/reducers/table';
+
+// tell direction after drag start, the first direction that reach 5px offset
+const DRAG_DIRECTION_NONE = '';
+const DRAG_DIRECTION_ROW = 'row';
+const DRAG_DIRECTION_COLUMN = 'column';
+
+const defaultDragState = {
+	column: -1,
+	row: -1,
+	startPoint: null,
+	direction: DRAG_DIRECTION_NONE, // row=move up down/column=move left right,
+	dropIndex: -1, // drag target
+};
+
+const useDraggableTable = ( table ) => {
+	const dispatch = useDispatch();
+	const { advanced_wp_table_data: tableData } = table;
+
+	let { rows = [] } = tableData;
+
+	const [ dragState, setDragState ] = useState( { ...defaultDragState } );
+
+	const rowsRef = useRef( [] ),
+		preview = useRef( null );
+
+	if ( dragState.direction === DRAG_DIRECTION_COLUMN ) {
+		rows = rows.map( ( x ) => offsetIndex( dragState.column, dragState.dropIndex, x ) );
+	}
+
+	if ( dragState.direction === DRAG_DIRECTION_ROW ) {
+		rows = offsetIndex( dragState.row, dragState.dropIndex, rows );
+	}
+
+	const setOpacity = ( i, j ) => {
+		let opacity = null;
+		const { direction, dropIndex } = dragState;
+
+		if ( DRAG_DIRECTION_COLUMN === direction ) {
+			if ( j === dropIndex ) {
+				opacity = 0.5;
+			} else {
+				opacity = 1;
+			}
+		}
+
+		if ( DRAG_DIRECTION_ROW === direction ) {
+			if ( i === dropIndex ) {
+				opacity = 0.5;
+			} else {
+				opacity = 1;
+			}
+		}
+
+		return opacity;
+	};
+
+	// https://stackoverflow.com/a/56063129/2647905
+	useEffect( () => {
+		rowsRef.current = rowsRef.current.slice( 0, rows.length );
+	}, [ rows ] );
+
+	const updateRows = ( newRows ) => {
+		const updatedTable = {
+			...table,
+			advanced_wp_table_data: {
+				...table.advanced_wp_table_data,
+				rows: newRows
+			}
+		};
+
+		dispatch( setTable( updatedTable ) );
+	};
+
+	const style = ( i, j ) => {
+		return {
+			cursor: dragState.direction ? 'move' : 'initial',
+			opacity: setOpacity( i, j ),
+		}
+	}
+
+	const onDragStart = ( e, i, j ) => {
+		e.dataTransfer.setDragImage( preview.current, 0, 0 );
+		// noinspection JSCheckFunctionSignatures
+		setDragState( {
+			...dragState,
+			row: i,
+			column: j,
+			startPoint: {
+				x: e.pageX,
+				y: e.pageY,
+			},
+		} );
+	}
+
+	const onDragEnter = ( i, j ) => {
+		if ( ! dragState.direction ) {
+			if ( dragState.column !== j ) {
+				setDragState( {
+					...dragState,
+					direction: DRAG_DIRECTION_COLUMN,
+					dropIndex: j,
+				} );
+				return;
+			}
+			if ( dragState.row !== i ) {
+				setDragState( {
+					...dragState,
+					direction: DRAG_DIRECTION_ROW,
+					dropIndex: i,
+				} );
+				return;
+			}
+			return;
+		}
+
+		if ( dragState.direction === DRAG_DIRECTION_COLUMN ) {
+			if ( j !== dragState.dropIndex ) {
+				setDragState( {
+					...dragState,
+					dropIndex: j,
+				} );
+			}
+			return;
+		}
+
+		if ( dragState.direction === DRAG_DIRECTION_ROW ) {
+			if ( i !== dragState.dropIndex ) {
+				setDragState( {
+					...dragState,
+					dropIndex: i,
+				} );
+			}
+		}
+	}
+
+	const onDragEnd = () => {
+		updateRows( rows );
+		setDragState( { ...defaultDragState } );
+	}
+
+	return [
+		rowsRef,
+		preview,
+		rows,
+		style,
+		onDragStart,
+		onDragEnter,
+		onDragEnd,
+	];
+}
+
+export default useDraggableTable;
