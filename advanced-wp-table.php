d43e3b954c49189b7c6a830ad313b3dba2d724a3
Mainul Hassan Main <shamim.moeen@gmail.com>
2023-05-24 18:16:56 +0600

    Make it ready for v1.3.0

--- advanced-wp-table.php
+++ advanced-wp-table.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mainulhassan.info
  * Text Domain:     advanced-wp-table
  * Domain Path:     /languages
- * Version:         1.2.1
+ * Version:         1.3.0
  * License:         GPLv3
  *
  * @package         Advanced_WP_Table
